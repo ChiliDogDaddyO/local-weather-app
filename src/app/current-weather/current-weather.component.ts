faa5b1b2fbdbada51736418fbf9e6fba112e40df
rox <qt02537@gmail.com>
2020-06-21 11:02:16 -0400

    null-guarding: safe navigation operator

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -12,14 +12,14 @@ import { WeatherService } from '../weather/weather.service'
 export class CurrentWeatherComponent implements OnInit {
   current: ICurrentWeather
   constructor(private httpClient: HttpClient, private weatherService: WeatherService) {
-    this.current = {
-      city: '',
-      country: '',
-      date: 0,
-      image: '',
-      temperature: 0,
-      description: '',
-    }
+    // this.current = {
+    //   city: '',
+    //   country: '',
+    //   date: 0,
+    //   image: '',
+    //   temperature: 0,
+    //   description: '',
+    // }
   }
 
   ngOnInit(): void {
